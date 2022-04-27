512f182fd483e1ecbb50d027179485935ffe4917
stepan <fisenko.stepan@mail.ru>
2022-04-27 21:41:28 +0300

    lesson_3_1

--- app/src/main/java/java/android/lesson_2_calculator/MainActivity.java
+++ app/src/main/java/java/android/lesson_2_calculator/MainActivity.java
@@ -3,12 +3,45 @@ package java.android.lesson_2_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        // numbers
+        findViewById(R.id.button0).setOnClickListener(this);
+        findViewById(R.id.button1).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
+        findViewById(R.id.button5).setOnClickListener(this);
+        findViewById(R.id.button6).setOnClickListener(this);
+        findViewById(R.id.button8).setOnClickListener(this);
+        findViewById(R.id.button9).setOnClickListener(this);
+        //findViewById(R.id.buttonDot).setOnClickListener(this);
+        // action
+        findViewById(R.id.buttonPlus).setOnClickListener(this);
+        findViewById(R.id.buttonMinus).setOnClickListener(this);
+        findViewById(R.id.buttonMultiply).setOnClickListener(this);
+        findViewById(R.id.buttonShare).setOnClickListener(this);
+        // result
+        findViewById(R.id.buttonEquals).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.button0: textView.setText("button0"); break;
+            case R.id.button1: textView.setText("button1"); break;
+        }
     }
 }
