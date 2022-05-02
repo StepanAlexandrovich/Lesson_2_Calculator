113d0021937699292e7596a8aa287871ee71c428
stepan <fisenko.stepan@mail.ru>
2022-05-02 16:09:48 +0300

    lesson_3_2_3_last

--- app/src/main/java/java/android/lesson_2_calculator/Logic.java
+++ app/src/main/java/java/android/lesson_2_calculator/Logic.java
@@ -1,10 +1,43 @@
 package java.android.lesson_2_calculator;
 
 public class Logic {
-    private String a,b,action,result;
+    private Value a = new Value();
+    private Value b = new Value();
+    private String action = "";
     private String text = "";
-    private boolean dotA,dotB; // защита от ввода второй точки в числе a или b
-    private boolean dot = false; // защита от ввода знака ( + - X / ) или (=) после точки
+
+    class Value{
+        String text = "",lastSymbol = "";
+        boolean dot;
+        int length;
+
+        void addSymbol(String symbol){
+            length++;
+            text += symbol;
+            lastSymbol = symbol;
+            if(symbol.equals(".")){ dot = true; }
+        }
+
+        void reset(){
+            text = "";
+            lastSymbol = "";
+            dot = false;
+            length = 0;
+        }
+
+        boolean ban(String symbol){
+            if(length == 7)                       { return true; } // ограничение на количество вводимых цифр в число
+            if(length == 6 && symbol.equals(".")) { return true; } // ограничение на количество вводимых цифр в число
+
+            if(text.equals("") && ( symbol.equals(".") || symbol.equals("0") ) ){ return true; } // запрет на ввод в начале числа 0 или .
+            if(symbol.equals(".") && dot){ return true; }                                        // запрет на ввод второй точки в число
+            return false;
+        }
+
+        boolean complete(){ return (!lastSymbol.equals(".") && !text.equals("")); } // число завершённое, после него можно ставить знаки +-*/=
+
+        String getText(){ return text; }
+    }
 
     /////// helpers to calculation ///////
     private float floatValue(String value){ return Float.parseFloat(value);  }
@@ -18,55 +51,39 @@ public class Logic {
     }
     //////////////////////////////////////////////
 
+
+    ///////////////// BASE METHODS ////////////////////
+
     void inputNumber(String number){
 
-        if(a==null) {
-            if(number.equals(".") || number.equals("0")){ return; }
-            a = number;
-            text = "";
-        } else
+        if( (a.getText()).equals("") && !a.ban(number) ){ text = ""; }  // автоматическое обнуление строки когда мы заного начинаем расчёт
 
-        if(action==null) {
-            if(dotA && number.equals(".")){ return; }
-
-            a+=number;
-            if(number.equals(".")){ dotA = true; }
-        } else
-
-        if(b == null) {
-            if(number.equals(".") || number.equals("0")){ return; }
-            b = number;
-        } else
-
-        if(result==null) {
-            if(dotB && number.equals(".")){ return; }
-
-            b+=number;
-            if(number.equals(".")){ dotB = true; }
+        if(action.equals("")) {
+            if(a.ban(number)) { return;  }
+            else  { a.addSymbol(number); }
         }
-
-        if(number.equals(".")){ dot = true;  } // защита от ввода знака ( + - X / ) или (=) после точки
-        else                  { dot = false; }
+        else{
+            if(b.ban(number)) { return;  }
+            else  { b.addSymbol(number); }
+        }
 
         text += number;
     }
 
     void inputAction(String action){
-        if(a!=null&&this.action==null&&!dot){
+        if(a.complete() && this.action.equals("")){
             this.action = action;
             text += " " + this.action + " ";
         }
     }
 
     void result(){
-        if(a!=null&&action!=null&&b!=null&&!dot){
-            text += " = "+action(action,floatValue(a),floatValue(b));
+        if(b.complete()){
+            text += " = "+action(action, floatValue(a.getText()), floatValue(b.getText()) );
 
-            a = null;
-            action = null;
-            b = null;
-            dotA = false;
-            dotB = false;
+            a.reset();
+            action = "";
+            b.reset();
         }
     }
 
